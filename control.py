5bfc0f4b51c9e369e74323a9b5af7c6cc31c43c4
Kumar-Neelamegam <kumar.neelamegam17@gmail.com>
2023-07-02 01:02:36 +0100

    WORKING API VERSION - Sucessfully created api in new_api.py - Customised control.py script - contorll using 192.168.1.166/api/bulbon or bulboff

--- control.py
+++ control.py
@@ -1,47 +1,25 @@
 import RPi.GPIO as GPIO
-import serial
-import os
-import can
-from can import Message
-import time
 
-GPIO.setmode(GPIO.BCM)
+class BulbControl:
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(5, GPIO.OUT)
+        GPIO.setup(6, GPIO.OUT)
+        GPIO.setup(13, GPIO.OUT)
+        GPIO.setup(16, GPIO.OUT)
+        GPIO.setup(19, GPIO.OUT)
+        GPIO.setup(20, GPIO.OUT)
 
-GPIO.setup(5,GPIO.OUT)
-GPIO.setup(6,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(16,GPIO.OUT)
-GPIO.setup(19,GPIO.OUT)
-GPIO.setup(20,GPIO.OUT)
+        GPIO.output(5, GPIO.LOW)
+        GPIO.output(6, GPIO.LOW)
+        GPIO.output(13, GPIO.LOW)
+        GPIO.output(16, GPIO.LOW)
+        GPIO.output(19, GPIO.LOW)
+        GPIO.output(20, GPIO.LOW)
+    
+    # Turning on bulb 1
+    def bulb_on(self):
+        GPIO.output(5, GPIO.HIGH)
 
-GPIO.output(5,GPIO.LOW)
-GPIO.output(6,GPIO.LOW)
-GPIO.output(13,GPIO.LOW)
-GPIO.output(16,GPIO.LOW)
-GPIO.output(19,GPIO.LOW)
-GPIO.output(20,GPIO.LOW)
-
-
-while True:
-	switch = input("<switch>: ")
-	func = input("0|1: ")
-	switch=int(switch)
-	func = int(func)
-
-	if switch == 1:
-		if func==0:
-			GPIO.output(5,GPIO.LOW)
-		elif func==1:
-			GPIO.output(5,GPIO.HIGH)
-		else:
-			pass
-
-	elif switch == 2:
-		if func==0:
-			GPIO.output(6,GPIO.LOW)
-		elif func==1:
-			GPIO.output(6,GPIO.HIGH)
-		else:
-			pass
-
-GPIO.cleanup()
+    def bulb_off(self):
+        GPIO.output(5, GPIO.LOW)
---
+++ new_api.py
@@ -0,0 +1,77 @@
+from flask import Flask, jsonify, request
+import os
+from control import BulbControl
+import RPi.GPIO as GPIO
+
+
+current_dir = os.getcwd()
+print("Current working directory:", current_dir)
+
+app = Flask(__name__)
+
+# Sample data - Replace this with your actual data or database integration
+data = [
+    {"id": 1, "name": "Item 1", "description": "Description of Item 1"},
+    {"id": 2, "name": "Item 2", "description": "Description of Item 2"},
+    {"id": 3, "name": "Item 3", "description": "Description of Item 3"}
+]
+
+# Endpoint to get all items
+@app.route('/api/items', methods=['GET'])
+def get_items():
+    return jsonify(data)
+
+@app.route('/api/bulbon', methods=['GET'])
+def get_bulbon():
+#    with open('smart_plug/control.py') as control:
+#        exec(control.read())
+    bulb = BulbControl()
+    bulb.bulb_on()
+    return jsonify("Executed sucessful..")
+
+@app.route('/api/bulboff', methods=['GET'])
+def get_bulboff():
+    bulb = BulbControl()
+    bulb.bulb_off()
+    return jsonify("Executed sucessful..")
+
+
+# Endpoint to get a specific item by ID
+@app.route('/api/items/<int:item_id>', methods=['GET'])
+def get_item(item_id):
+    item = next((item for item in data if item['id'] == item_id), None)
+    if item:
+        return jsonify(item)
+    else:
+        return jsonify({"message": "Item not found"}), 404
+
+# Endpoint to add a new item
+@app.route('/api/items', methods=['POST'])
+def add_item():
+    new_item = request.json
+    new_item['id'] = len(data) + 1
+    data.append(new_item)
+    return jsonify(new_item), 201
+
+# Endpoint to update an existing item by ID
+@app.route('/api/items/<int:item_id>', methods=['PUT'])
+def update_item(item_id):
+    item = next((item for item in data if item['id'] == item_id), None)
+    if item:
+        item.update(request.json)
+        return jsonify(item)
+    else:
+        return jsonify({"message": "Item not found"}), 404
+
+# Endpoint to delete an item by ID
+@app.route('/api/items/<int:item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    global data
+    data = [item for item in data if item['id'] != item_id]
+    return jsonify({"message": "Item deleted successfully"}), 200
+
+if __name__ == '__main__':
+#    app.run(debug=True)
+    custom_ip = '192.168.1.166'
+    custom_port = 8080
+    app.run(host=custom_ip, port=custom_port, debug=True)

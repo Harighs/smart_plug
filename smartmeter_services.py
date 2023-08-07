d751372f573ee4eed74479027d1c813e9bf91688
harighs <harishankarghs@gmail.com>
2023-08-08 00:14:33 +0200

    working smartmeter_service

--- smartmeter_services.py
+++ smartmeter_services.py
@@ -3,15 +3,24 @@ import pandas as pd
 
 
 class SmartMeter():
-    def __init__(self):           
+    def __init__(self):
+        """
+        This class is used to fetch data from the smartmeter.netz-noe.at website.
+        
+        Input: None
+        
+        Output:
+            DataFrame: Returns a DataFrame with the following from json response from the website
+            
+        Usage:
+            smartmeter = SmartMeter() --> Returns a SmartMeter object as Dataframe which can be stored
+        """   
         # URL for authentication
         self.auth_url = 'https://smartmeter.netz-noe.at/orchestration/Authentication/Login'
         self.auth_payload = {"user": "SommererPrivatstiftung", "pwd": "SpS*1996"}
-
-    def post_request(self):
-        self.auth_response = requests.post(self.auth_url, json=self.auth_payload)
-                
-        if self.auth_response.status_code == 200:
+        
+        if self.post_request(self):
+            print("Authentication successful")
             auth_cookie = self.auth_response.cookies['__Host-go4DavidSecurityToken']
             auth_xsrf_token = self.auth_response.cookies['XSRF-Token']
 
@@ -22,16 +31,29 @@ class SmartMeter():
                 'Cookie': f'__Host-go4DavidSecurityToken={auth_cookie}; XSRF-Token={auth_xsrf_token}',
             }
             
-            data_response = requests.get(data_url, headers=headers)
             
-            if data_response.status_code == 200:
-                data = data_response.json()
-                # Process the data as needed
-                df = pd.DataFrame(data[0])
-                return df
+            if self.data_response(self, data_url, headers):
+                data_response = requests.get(data_url, headers=headers).json()
+                return pd.DataFrame(data_response[0])
             else:
                 print("Data request failed with status code:", data_response.status_code)
                 return None
         else:
             print("Authentication failed with status code:", self.auth_response.status_code)
             return None
+            
+
+    @staticmethod
+    def post_request(self):
+        self.auth_response = requests.post(self.auth_url, json=self.auth_payload)
+        if self.auth_response.status_code == 200:
+            return True
+        
+    @staticmethod
+    def data_response(self, data_url, headers):
+        data_response = requests.get(data_url, headers=headers)
+        if data_response.status_code == 200:
+            return True
+        else:
+            return False
+                

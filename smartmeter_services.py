51d8beef4f347dbf519cac764aa2f7b05bc3e025
harighs <harishankarghs@gmail.com>
2023-08-26 10:25:59 +0200

    Exceptional Handling

--- smartmeter_services.py
+++ smartmeter_services.py
@@ -16,64 +16,55 @@ class SmartMeter():
         Usage:
             smartmeter = SmartMeter() --> Returns a SmartMeter object as Dataframe which can be stored
         """   
-        # URL for authentication
-        # self.auth_url = 'https://smartmeter.netz-noe.at/orchestration/Authentication/Login'
-        # self.auth_payload = {"user": "SommererPrivatstiftung", "pwd": "SpS*1996"}
-        
-        # if self.post_request(self):
-        #     print("Authentication successful")
-        #     auth_cookie = self.auth_response.cookies['__Host-go4DavidSecurityToken']
-        #     auth_xsrf_token = self.auth_response.cookies['XSRF-Token']
-
-        #     # URL for fetching data
-        #     data_url = f"https://smartmeter.netz-noe.at/orchestration/User/GetMeteringPointByAccountId?accountId=000019014443"
-            
-        #     headers = {
-        #         'Cookie': f'__Host-go4DavidSecurityToken={auth_cookie}; XSRF-Token={auth_xsrf_token}',
-        #     }
-            
-        #     if self.data_response(self, data_url, headers):
-        #         data_response = requests.get(data_url, headers=headers).json()
-        #         self.SMART_METER_DATA = pd.DataFrame(data_response[0])
-        #         (self.SMART_METER_DATA)
-        #         print("HARI ---- SUCESS#######################")
-        #     else:
-        #         print("Data request failed with status code:", 'DATA FAILURE')
-        # else:
-        #     print("Authentication failed with status code:", self.auth_response.status_code)
+        self.auth_url = 'https://smartmeter.netz-noe.at/orchestration/Authentication/Login'
+        self.auth_payload = {"user": "SommererPrivatstiftung", "pwd": "SpS*1996"}
         return None
     
-    def get_data(self):
-        auth_url = 'https://smartmeter.netz-noe.at/orchestration/Authentication/Login'
-        auth_payload = {"user": "SommererPrivatstiftung", "pwd": "SpS*1996"}
-
-        auth_response = requests.post(auth_url, json=auth_payload)
-        auth_cookie = auth_response.cookies['__Host-go4DavidSecurityToken']
-        auth_xsrf_token = auth_response.cookies['XSRF-Token']
-        ### TODO: update date from mobile 
+    def get_data(self, intrested_date: str = None):
+        """
+        Usage:
+            intrested_date: str --> '2023-8-24'
         
-        data_url = f"https://smartmeter.netz-noe.at/orchestration/ConsumptionRecord/Day?meterId=AT0020000000000000000000020826368&day=2023-8-24&__Host-go4DavidSecurityToken={auth_cookie}"
-
+        Output:
+            DataResonse['meteredValues'].sum() --> [float] kWh
+        """
+        
+        auth_cookie, auth_xsrf_token = self.post_request(self)
+        
+        if auth_cookie and auth_xsrf_token is None:
+            print("Authentication failed with status code:", self.auth_response.status_code)
         headers = {
             'Cookie': f'__Host-go4DavidSecurityToken={auth_cookie}; XSRF-Token={auth_xsrf_token}',
         }
+        if intrested_date is None:
+            intrested_date = '2023-8-24'
+            
+        ### TODO: update date from mobile 
+        self.data_url = f"https://smartmeter.netz-noe.at/orchestration/ConsumptionRecord/Day?meterId=AT0020000000000000000000020826368&day={intrested_date}&__Host-go4DavidSecurityToken={auth_cookie}"
+        data_response = self.data_response(self, self.data_url, headers)
+        if data_response is not None:
+            data_response = pd.DataFrame(data_response)
+            return data_response['meteredValues'].sum()
 
-        data_response = requests.get(data_url, headers=headers).json()
-        data = pd.DataFrame(data_response)
-        data = data['meteredValues'].sum()
-        return data
     
     @staticmethod
     def post_request(self):
-        self.auth_response = requests.post(self.auth_url, json=self.auth_payload)
-        if self.auth_response.status_code == 200:
-            return True
+        auth_response = requests.post(self.auth_url, json=self.auth_payload)
+        if auth_response.status_code == 200:
+            print("Authentication successful...")
+            auth_cookie = auth_response.cookies['__Host-go4DavidSecurityToken']
+            auth_xsrf_token = auth_response.cookies['XSRF-Token']
+            return auth_cookie, auth_xsrf_token
+        else:
+            raise Exception("Authentication failed with status code: Smartmeter_service line 74", self.auth_response.status_code)
+            
         
     @staticmethod
     def data_response(self, data_url, headers):
-        data_response = requests.get(data_url, headers=headers)
+        data_response = requests.get(data_url, headers=headers).json()
         if data_response.status_code == 200:
-            return True
+            return data_response
         else:
-            return False
+            print("Data request failed with status code:", 'DATA FAILURE')
+            raise Exception("Data request failed with status code: Smartmeter_service line 84", data_response.status_code)
                 

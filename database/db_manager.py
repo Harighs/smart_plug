1f5e8625c3d8306511ad82391b46be9f5698e0f6
Kumar <kumar.neelamegam17@gmail.com>
2023-09-29 02:59:17 +0200

    Updated the database included Reports fields

--- database/db_manager.py
+++ database/db_manager.py
@@ -33,16 +33,21 @@ def create_table():
             smart_meter_consumption TEXT NOT NULL,
             awattar_unit TEXT NOT NULL,
             smart_meter_unit TEXT NOT NULL,
+            R1 TEXT NOT NULL,
+            R2 TEXT NOT NULL,
+            R3 TEXT NOT NULL,
+            R4 TEXT NOT NULL,
+            R5 TEXT NOT NULL,
             status BOOLEAN
         )
     ''')
     cursor.close()
 
-def insert_table(start_datetime, end_datetime, awattar_price, smart_meter_consumption):
+def insert_table(start_datetime, end_datetime, awattar_price, smart_meter_consumption, R2, R3, R5):
     conn = sqlite3.connect(db_name)
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO datacache (start_datetime, end_datetime, awattar_price, smart_meter_consumption, awattar_unit, smart_meter_unit, status) VALUES (?, ?, ?, ?, ?, ?, ?)", 
-                   (start_datetime, end_datetime, awattar_price, smart_meter_consumption, '€', 'Eur/MWh', 'true'))
+    cursor.execute("INSERT INTO datacache (start_datetime, end_datetime, awattar_price, smart_meter_consumption, awattar_unit, smart_meter_unit, R1, R2, R3, R4, R5, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+                   (start_datetime, end_datetime, awattar_price, smart_meter_consumption, '€', 'Eur/MWh', awattar_price, R2, R3, smart_meter_consumption, R5, 'true'))
     conn.commit()
     cursor.close()
 
@@ -65,6 +70,6 @@ def delete_table():
 if __name__ == '__main__':
     create_connection()
     create_table()
-    insert_table("2023-09-18 01:00:00", "2023-09-18 02:00:00", "5.50", "100") # TODO pass it from services
+    insert_table("2023-09-18 01:00:00", "2023-09-18 02:00:00", "5.50", "100", "0", "0", "0") # TODO pass it from services
     read_table()
     #delete_table()

9abfdafcd64909b684034e7fd075f7223d832232
Kumar-Neelamegam <kumar.neelamegam17@gmail.com>
2023-10-27 16:19:34 +0200

    Tested automode manually and it worked

--- main_services/auto_mode.py
+++ main_services/auto_mode.py
@@ -4,6 +4,7 @@ sys.path.append('/home/pi/smart_plug/')
 
 import datetime
 import sqlite3
+import pandas as pd
 
 from external_services.awattar_services import AwattarServices
 from database.db_manager import DatabaseManager
@@ -14,16 +15,19 @@ from pi_controller.relay_controller import RelayControl
 from main_services.common_utils import common_utils 
 
 # this service should restart every 30 minutes
-
-
 class Auto_Mode:
     def __init__(self):
         self.future_df = None
         pass
 
-    def auto_mode(self):
+    def auto_mode(self, relayNumber):
         db = DatabaseManager()
-        times_toturn_on = db.read_automode_24hrs_before(1) # relay1
+        times_toturn_on = db.read_automode_24hrs_before(relayNumber)
+        if not times_toturn_on:
+            return
+        else:
+            pass
+        
         times_toturn_on = int(times_toturn_on[0][0])
 
         self.future_df = AwattarServices().AWATTAR_FUTURE_PRICE()
@@ -39,58 +43,52 @@ class Auto_Mode:
         # add extra columns to data frame and to match database table "automaterelay"
         self.future_df['triggerstatus'] = True
         self.future_df['unit'] = "kWh"
-        self.future_df['relaynumber'] = 1 #relay1
+        self.future_df['relaynumber'] = relayNumber
 
         print(self.future_df)
 
         if len(self.future_df) > 0:
-            print("Found matching auto mode")
-             # Feed Master Data CSV to the database
+            print("Found matching auto mode for relay:", relayNumber)
             conn = sqlite3.connect("database/"+common_utils.static_database_filename)
             self.future_df.to_sql('automaterelay', conn, index=False, if_exists='replace') # replace the dataset
             self.future_df.to_sql('automaterelay_report', conn, index=False, if_exists='append') # for report purpose
         else:
             print("No matching auto mode")
 
-        self.turn_on_turn_off()
+        self.turn_on_turn_off(relayNumber)
 
         return self.future_df
 
-    def turn_on_turn_off(self):
+    def turn_on_turn_off(self, relayNumber):
         # check whether the relay 1 or 2 is on Auto mode then turn on and turn off automatically
         db = DatabaseManager()
-        results1 = db.read_relaymode_temp(1) # setting relay1 
-        results2 = db.read_relaymode_temp(2) # setting relay2
-        if len(results1) > 0: # check whether relay1 is on Auto Mode
-                print(self.future_df)
+        results = db.read_relaymode_temp(relayNumber) # setting relay number
+
+        conn = sqlite3.connect("database/"+common_utils.static_database_filename)
+        query = f"SELECT * from automaterelay where relaynumber={relayNumber}"
+        new_dataframe = pd.read_sql_query(query, conn)
+        print(new_dataframe)
+
+        if len(results) > 0: # check whether relay is on Auto Mode
                 current_time = datetime.datetime.now()
                 # Check if current time is within any interval
                 while True:
-                    for index, row in self.future_df.iterrows():
-                        if not row['start_timestamp'] <= current_time <= row['end_timestamp']:
+                    for index, row in new_dataframe.iterrows():
+                        startdatetime = pd.to_datetime(row['start_timestamp'])
+                        enddatetime = pd.to_datetime(row['end_timestamp'])
+                        if startdatetime <= current_time <= enddatetime:
                             # Turn On
                             RelayControl().relayController(relayNumber=1, relayStatus=1)
+                            print(f"Relay {relayNumber} turned on in AutoMode")
                         else:
                             # Turn OFF
                             RelayControl().relayController(relayNumber=1, relayStatus=0)
-        elif len(results2) > 0: # check whether relay2 is on Auto Mode
-                print(self.future_df)
-                current_time = datetime.datetime.now()
-                # Check if current time is within any interval
-                while True:
-                    for index, row in self.future_df.iterrows():
-                        if not row['start_timestamp'] <= current_time <= row['end_timestamp']:
-                            # Turn On
-                            RelayControl().relayController(relayNumber=2, relayStatus=1)
-                        else:
-                            # Turn OFF
-                            RelayControl().relayController(relayNumber=2, relayStatus=0)
-    
+                            print(f"Relay {relayNumber} turned off in AutoMode")
+                    break
         else:
             return
 
 if __name__ == "__main__":
     auto_mode = Auto_Mode()
-    auto_mode.auto_mode()
-    #auto_mode.turn_on_turn_off()
-    #print(auto_mode.future_df)
+    auto_mode.auto_mode(1)
+    auto_mode.auto_mode(2)
